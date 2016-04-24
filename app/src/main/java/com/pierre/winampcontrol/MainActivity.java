723c7dad77001876a449368c5ebc64c005b29d22
Pierre Rispal <Pierre Rispal>
2016-04-24 11:20:11 +0200

    pause working, only need to do the rest

---
+++ app/src/main/java/com/pierre/winampcontrol/MainActivity.java
@@ -0,0 +1,69 @@
+package com.pierre.winampcontrol;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.net.URISyntaxException;
+
+import io.socket.client.IO;
+import io.socket.client.Socket;
+import io.socket.emitter.Emitter;
+
+public class MainActivity extends AppCompatActivity {
+    private Socket socket;
+    {
+        try{
+            socket = IO.socket("http://192.168.0.40:3000/");
+        }catch(URISyntaxException e){
+            //@TODO: if no connection we should block the rest of the app
+            Log.i("channel", "noconnection");
+            throw new RuntimeException(e);
+        }
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        socket.connect();
+        Button pause=(Button)findViewById(R.id.button_pause);
+        pause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendAction("PAUSE");
+            }
+        });
+
+    }
+    @Override
+    public void onBackPressed(){
+        super.onBackPressed();
+        this.finish();
+    }
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        socket.disconnect();
+    }
+    public void sendAction(String action){
+        JSONObject send = new JSONObject();
+        try {
+            send.put("command", action);
+            //send.put("msg", message.getText().toString());
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        Log.i("json", send.toString());
+        socket.emit("command", send);
+    }
+}
